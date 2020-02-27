1d6d3488f52ee76e8fb79e2c34ac448f635f4151
MaliSarus <43217693+MaliSarus@users.noreply.github.com>
2020-02-27 18:07:51 +0300

    almost done

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,17 +1,43 @@
 $(window).ready(function () {
-    $('.specialists__slider').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
+    // $('.specialists__slider').slick({
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     dots: false,
+    //     arrows:false,
+    //     adaptiveHeight: true
+    // });
+    //
+    // $('.specialists__control-panel .arrows__arrow-left').on('click',function () {
+    //     $('.specialists__slider').slick('slickPrev');
+    // });
+    //
+    // $('.specialists__control-panel .arrows__arrow-right').on('click',function () {
+    //     $('.specialists__slider').slick('slickNext');
+    // });
+    $('.specialists__slider.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        items:4,
+        autoWidth:true,
+        stagePadding: 20,
+        nav: false,
         dots: false,
-        arrows:false
+        onRefresh: function () {
+            $('.specialists__slider.owl-carousel').find('div.owl-item').height('');
+        },
+        onRefreshed: function () {
+            $('.specialists__slider.owl-carousel').find('div.owl-item').height($('.specialists__slider.owl-carousel').height())
+        }
     });
 
-    $('.specialists__arrow-left').on('click',function () {
-        $('.specialists__slider').slick('slickPrev');
+    $('.feedback__slider.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        items:2,
+        nav: false,
+        dots: false,
     });
 
-    $('.specialists__arrow-right').on('click',function () {
-        $('.specialists__slider').slick('slickNext');
-    });
+
 });
