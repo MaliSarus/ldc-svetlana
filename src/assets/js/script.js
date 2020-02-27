b7a79d73479aed76673e114653ebdcfde76a274c
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-02-27 19:57:26 +0300

    complete specialists slider

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -15,29 +15,47 @@ $(window).ready(function () {
     // $('.specialists__control-panel .arrows__arrow-right').on('click',function () {
     //     $('.specialists__slider').slick('slickNext');
     // });
-    $('.specialists__slider.owl-carousel').owlCarousel({
-        loop:true,
-        margin:20,
-        items:4,
-        autoWidth:true,
-        stagePadding: 20,
+    const specOwl = $('.specialists__slider.owl-carousel');
+    specOwl.owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 4,
         nav: false,
         dots: false,
-        onRefresh: function () {
-            $('.specialists__slider.owl-carousel').find('div.owl-item').height('');
-        },
-        onRefreshed: function () {
-            $('.specialists__slider.owl-carousel').find('div.owl-item').height($('.specialists__slider.owl-carousel').height())
-        }
     });
 
-    $('.feedback__slider.owl-carousel').owlCarousel({
-        loop:true,
-        margin:20,
-        items:2,
+    $('.specialists__control-panel .arrows .arrows__arrow-left').on('click', function () {
+        specOwl.trigger('prev.owl.carousel');
+    });
+    $('.specialists__control-panel .arrows .arrows__arrow-right').on('click', function () {
+        specOwl.trigger('next.owl.carousel');
+    });
+
+    const feedOwl = $('.feedback__slider.owl-carousel');
+
+    feedOwl.owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 2,
         nav: false,
         dots: false,
+    });
+
+    $('.feedback__control-panel .arrows .arrows__arrow-left').on('click', function () {
+        feedOwl.trigger('prev.owl.carousel');
+    });
+    $('.feedback__control-panel .arrows .arrows__arrow-right').on('click', function () {
+        feedOwl.trigger('next.owl.carousel');
     });
 
 
 });
+
+$(window).scroll(parallaxScrolling);
+
+function parallaxScrolling() {
+    var scrolled = $(window).scrollTop();
+    $('.layer1').css('top', (0 - (scrolled * .9)) + 'px');
+    $('.layer2').css('top', (0 - (scrolled * .66)) + 'px');
+    $('.layer3').css('top', (0 - (scrolled * .33)) + 'px');
+}
